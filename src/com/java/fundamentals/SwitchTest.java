f8996f86e2a5633398478ff96e0301a49c1f147f
Angasalavikas <angasalavikas123@gmail.com>
2026-04-28 14:58:43 +0530

    updated SwitchTest

---
+++ src/com/java/fundamentals/SwitchTest.java
@@ -0,0 +1,40 @@
+package com.java.fundamentals;
+import java.util.Scanner;
+
+public class SwitchTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // Lowercase 'sc'
+
+        if (sc.hasNextInt()) {
+            int day = sc.nextInt(); // Use lowercase 'sc' here too
+
+            switch(day) {
+                case 1:
+                System.out.println("Monday");
+                    break;
+                case 2:
+                    System.out.println("Tuesday");
+                    break;
+                case 3:
+                    System.out.println("Wednesday");
+                    break;
+                case 4:
+                    System.out.println("Thursday");
+                    break;
+                case 5:
+                    System.out.println("Friday");
+                    break;
+                case 6:
+                    System.out.println("Saturday");
+                    break;
+                case 7:
+                    System.out.println("Sunday");
+                    break;
+                default:
+                    System.out.println("Invalid output");
+                    break;
+            }
+        }
+        sc.close();
+    }
+}
