461a167d657f7228ab5d64c82e617cff9097dadf
Angasalavikas <angasalavikas123@gmail.com>
2026-05-03 14:55:29 +0530

    updated SwitchTest

--- src/com/java/fundamentals/SwitchTest.java
+++ src/com/java/fundamentals/SwitchTest.java
@@ -10,25 +10,40 @@ public class SwitchTest {
 
             switch(day) {
                 case 1:
-                System.out.println("Monday");
+                System.out.println("January");
                     break;
                 case 2:
-                    System.out.println("Tuesday");
+                    System.out.println("feb");
                     break;
                 case 3:
-                    System.out.println("Wednesday");
+                    System.out.println("march");
                     break;
                 case 4:
-                    System.out.println("Thursday");
+                    System.out.println("April");
                     break;
                 case 5:
-                    System.out.println("Friday");
+                    System.out.println("may");
                     break;
                 case 6:
-                    System.out.println("Saturday");
+                    System.out.println("June");
                     break;
                 case 7:
-                    System.out.println("Sunday");
+                    System.out.println("July");
+                    break;
+                case 8:
+                    System.out.println("August");
+                    break;
+                case 9:
+                    System.out.println("September");
+                    break;
+                case 10:
+                    System.out.println("October");
+                    break;
+                case 11:
+                    System.out.println("November");
+                    break;
+                case 12:
+                    System.out.println("December3");
                     break;
                 default:
                     System.out.println("Invalid output");
