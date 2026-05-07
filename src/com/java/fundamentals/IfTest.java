6dcb697eac05cea9adc7853ba2863bd36133b199
Angasalavikas <angasalavikas123@gmail.com>
2026-05-07 12:57:43 +0530

    updated IfTest.java

--- src/com/java/fundamentals/IfTest.java
+++ src/com/java/fundamentals/IfTest.java
@@ -6,14 +6,11 @@ public class IfTest {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        if(num > 0){
+        if(num >= 0){
             System.out.println("positive");
         }
-        else if(num < 0){
+        else {
             System.out.println("negative");
-        }
-        else{
-            System.out.println("Zero");
         }
         sc.close();
     }
