29219b0504fa81ed1820252748764e3cbb68f34b
Angasalavikas <angasalavikas123@gmail.com>
2026-05-09 16:22:35 +0530

    initial commit VowelsAndConsonants.java

---
+++ src/com/java/fundamentals/VowelsAndConsonants.java
@@ -0,0 +1,26 @@
+package com.java.fundamentals;
+
+import java.util.Scanner;
+
+public class VowelsAndConsonants {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the String :");
+        String str = sc.nextLine();
+        int vowels = 0;
+        int consonants = 0;
+        for ( int i = 0; i < str.length(); i++) {
+            // for (int i = 0; i < str.length(); i++)
+            char ch = Character.toLowerCase(str.charAt(i));
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                vowels++;
+            } else if (ch >= 'a' && ch <= 'z') {
+                consonants++;
+            }
+        }
+            System.out.println(" Number of Vowels = " + vowels);
+            System.out.println("Number of Consonants = " + consonants);
+
+        sc.close();
+    }
+}
