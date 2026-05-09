001388e12b36f753cb8f274c65fdc0a98561c641
Angasalavikas <angasalavikas123@gmail.com>
2026-05-09 16:24:03 +0530

    initial commit VowelsAndConsonants.java

--- src/com/java/fundamentals/VowelsAndConsonants.java
+++ src/com/java/fundamentals/VowelsAndConsonants.java
@@ -10,7 +10,6 @@ public class VowelsAndConsonants {
         int vowels = 0;
         int consonants = 0;
         for ( int i = 0; i < str.length(); i++) {
-            // for (int i = 0; i < str.length(); i++)
             char ch = Character.toLowerCase(str.charAt(i));
             if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
                 vowels++;
