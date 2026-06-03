26a574a70ef35da8567c9a8a941368fd5a9dcda3
Angasalavikas <angasalavikas123@gmail.com>
2026-06-03 10:32:43 +0530

    initial commit studentTotalMarks.java

---
+++ src/com/java/fundamentals/studentTotalMarks.java
@@ -0,0 +1,19 @@
+package com.java.fundamentals;
+
+import java.util.Scanner;
+public class studentTotalMarks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the sub1 marks: ");
+        int sub1marks = sc.nextInt();
+        System.out.println("enter the sub2 marks: ");
+        int sub2marks = sc.nextInt();
+        System.out.println("enter the sub3 marks: ");
+        int sub3marks = sc.nextInt();
+        System.out.println("enter the sub4 marks: ");
+        int sub4marks = sc.nextInt();
+        int total = sub1marks + sub2marks + sub3marks + sub4marks;
+        System.out.println("total marks: " + total);
+
+    }
+}
