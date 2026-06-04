24f60d4ba86840d77564cee9529d559d5f4a4102
Angasalavikas <angasalavikas123@gmail.com>
2026-06-04 18:24:52 +0530

    initial commit studentTotalMarks.java

---
+++ src/com/java/fundamentals/NaturalNumber.java
@@ -0,0 +1,15 @@
+package com.java.fundamentals;
+
+import java.util.Scanner;
+
+public class NaturalNumber {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value :");
+        int num = sc.nextInt();
+        System.out.println("print natural numbers:");
+        for (int i = 1; i <=num; i++){
+            System.out.println(i);
+        }
+    }
+}
