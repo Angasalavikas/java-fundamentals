99b2b8a6913bdf9a7a34e465c16abb71cdd11e96
Angasalavikas <angasalavikas123@gmail.com>
2026-06-09 22:11:45 +0530

    initial commit SumOfNaturalNumbers.java

--- src/com/java/fundamentals/SumOfNaturalNumbers.java
+++ src/com/java/fundamentals/SumOfNaturalNumbers.java
@@ -2,16 +2,15 @@ package com.java.fundamentals;
 import java.util.Scanner;
 
 public class SumOfNaturalNumbers {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
-        int n = sc.nextInt();
-        int sum = 0;
-        for(int i=1;i<=n;i++){
-            sum = sum + i;
-        }
-        System.out.println("Sum = " + sum);
-
+       int N = sc.nextInt();
+       int sum = 0;
+       for(int i = 0;i <= N;i++){
+           sum = sum +i;
+       }
+        System.out.println(sum);
     }
 }
