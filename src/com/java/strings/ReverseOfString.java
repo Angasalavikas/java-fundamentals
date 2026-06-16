5ead7923809799ddfad1e3e737a9f7b4f25aa81a
Angasalavikas <angasalavikas123@gmail.com>
2026-06-16 21:41:55 +0530

    initial commit ReverseOfString.java

---
+++ src/com/java/strings/ReverseOfString.java
@@ -0,0 +1,14 @@
+package com.java.strings;
+
+import java.util.Scanner;
+
+public class ReverseOfString {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the String:");
+        String str = sc.nextLine();
+        for(int i = str.length()-1;i >=0;i--){
+            System.out.print(str.charAt(i));
+        }
+    }
+}
