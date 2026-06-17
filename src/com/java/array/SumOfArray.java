a4e5f08e6faf7bdbf9a72e216998be9b325b1afc
Angasalavikas <angasalavikas123@gmail.com>
2026-06-17 22:51:58 +0530

    initial commit SumOfArray.java

---
+++ src/com/java/array/SumOfArray.java
@@ -0,0 +1,13 @@
+package com.java.array;
+
+public class SumOfArray {
+    static void main() {
+        int[] arr = {10, 20, 30, 40, 50};
+        int sum = 0;
+        for(int i = 0;i < arr.length;i++){
+            sum += arr[i];
+        }
+        System.out.println("sum: " + sum);
+    }
+
+}
