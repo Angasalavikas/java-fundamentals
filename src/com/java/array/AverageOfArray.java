873a9cee1f3453a0a511bb6fb52016151bae3795
Angasalavikas <angasalavikas123@gmail.com>
2026-06-17 22:52:58 +0530

    initial commit AverageOfArray.java

---
+++ src/com/java/array/AverageOfArray.java
@@ -0,0 +1,13 @@
+package com.java.array;
+
+public class AverageOfArray {
+    static void main() {
+        int[] arr = {10,20,30,40,50};
+        int sum = 0;
+        for(int i = 0;i < arr.length;i++){
+            sum += arr[i];
+        }
+        double average = (double) sum / arr.length;
+        System.out.println("average: " + average);
+    }
+}
