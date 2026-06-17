a66b5270b41e25d320171cb80c613be905cbf1bb
Angasalavikas <angasalavikas123@gmail.com>
2026-06-17 22:54:16 +0530

    initial commit ReverseOfArrayWithoutAnotherArray.java

---
+++ src/com/java/array/ReverseOfArrayWithoutAnotherArray.java
@@ -0,0 +1,19 @@
+package com.java.array;
+
+public class ReverseOfArrayWithoutAnotherArray {
+    static void main() {
+        int[] arr = {10,20,30,40,50,60};
+        int start = 0;
+        int end = arr.length-1;
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        for(int num : arr){
+            System.out.println(num + "");
+        }
+    }
+}
