d073dd5fa4159eeec92e3fb51d9f6148b63a0471
Angasalavikas <angasalavikas123@gmail.com>
2026-06-18 21:34:16 +0530

    initial commit CountEvenAndOddInArray.java

--- src/com/java/array/SumOfArray.java
+++ src/com/java/array/SumOfArray.java
@@ -5,6 +5,7 @@ public class SumOfArray {
         int[] arr = {10, 20, 30, 40, 50};
         int sum = 0;
         for(int i = 0;i < arr.length;i++){
+
             sum += arr[i];
         }
         System.out.println("sum: " + sum);
