e8c624be19bb011140a14f1e8b383f768162c3f4
Angasalavikas <angasalavikas123@gmail.com>
2026-06-18 21:33:37 +0530

    initial commit CountEvenAndOddInArray.java

--- src/com/java/array/CountEvenAndOddInArray.java
+++ src/com/java/array/CountEvenAndOddInArray.java
@@ -8,7 +8,8 @@ public class CountEvenAndOddInArray {
         for(int i = 0;i < arr.length;i++){
             if(arr[i] % 2 == 0){
                 even++;
-            }else {
+            }
+            else {
                 odd++;
             }
         }
