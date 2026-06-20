0b9309b7a4c3fc29438e2f48cc811a7f994a55c1
Angasalavikas <angasalavikas123@gmail.com>
2026-06-20 23:06:38 +0530

    initial commit MinAndMAxInASingleArray.java

---
+++ src/com/java/array/MinAndMAxInASingleArray.java
@@ -0,0 +1,19 @@
+package com.java.array;
+
+public class MinAndMAxInASingleArray {
+    static void main() {
+        int[] arr = {10,20,30,40,50,60};
+        int min = arr[0];
+        int max = arr[0];
+        for(int i = 0;i < arr.length;i++){
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        System.out.println(" max array Element: " + max);
+        System.out.println("min array element: " + min);
+    }
+}
