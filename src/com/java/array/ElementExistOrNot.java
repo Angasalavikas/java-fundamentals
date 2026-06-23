b00682d7e96dd7b5ebcf001f71ae1fd2126cbba1
Angasalavikas <angasalavikas123@gmail.com>
2026-06-23 23:00:36 +0530

    initial commit ElementExistOrNot.java

--- src/com/java/array/ElementExistOrNot.java
+++ src/com/java/array/ElementExistOrNot.java
@@ -3,7 +3,7 @@ package com.java.array;
 public class ElementExistOrNot {
     static void main() {
         int[] arr = {10,43,35,43,34};
-        int key = 30;
+        int key = 35;
         boolean found = false;
         for(int i = 0;i < arr.length;i++){
             if(arr[i] == key){
