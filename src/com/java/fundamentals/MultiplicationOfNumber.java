5768b81c9a7f6f117c3aaadb4fa5a6a10a856910
Angasalavikas <angasalavikas123@gmail.com>
2026-06-27 18:58:56 +0530

    initial commit MultiplicationOfNumber.java

--- src/com/java/fundamentals/MultiplicationOfNumber.java
+++ src/com/java/fundamentals/MultiplicationOfNumber.java
@@ -7,7 +7,7 @@ public class MultiplicationOfNumber {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the value:");
         int N = sc.nextInt();
-        System.out.println("print multiplication:" + N);
+        System.out.println("print multiplication:");
         for (int i = 0; i <= 10; i++) {
             System.out.println(N + "x" + i + "=" + (N*i));
         }
