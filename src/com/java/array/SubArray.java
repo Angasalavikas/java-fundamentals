fc94efa26bf51fe6047898eb9e7b70cb555734a8
Angasalavikas <angasalavikas123@gmail.com>
2026-07-19 22:27:45 +0530

    intial commit SubArray.java

--- src/com/java/array/SubArray.java
+++ src/com/java/array/SubArray.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class SubArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
