1bfa5cc4efa495b5ad3fd4b31e659a2999b8afa8
Angasalavikas <angasalavikas123@gmail.com>
2026-07-21 22:59:13 +0530

    intial commit Anagram.java

--- src/com/java/strings/Anagram.java
+++ src/com/java/strings/Anagram.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class Anagram {
     static void main(String[] args) {
         String str1 = "vikas";
-        String str2 = "balu";
+        String str2 = "vikas";
         char[] a = str1.toCharArray();
         char[] b = str2.toCharArray();
         Arrays.sort(a);
