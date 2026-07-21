12f3af10c67281bc30c5cd2fe6ed216e4190409e
Angasalavikas <angasalavikas123@gmail.com>
2026-07-21 22:58:29 +0530

    intial commit Anagram.java

---
+++ src/com/java/strings/Anagram.java
@@ -0,0 +1,20 @@
+package com.java.strings;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Anagram {
+    static void main(String[] args) {
+        String str1 = "vikas";
+        String str2 = "balu";
+        char[] a = str1.toCharArray();
+        char[] b = str2.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        if(Arrays.equals(a,b)){
+            System.out.println("anagram");
+        }else{
+            System.out.println("not an anagram");
+        }
+    }
+}
