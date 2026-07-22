d9d43783a13e5b211c1c8c193193b212b025cdb7
Angasalavikas <angasalavikas123@gmail.com>
2026-07-22 23:09:22 +0530

    intial commit Palindrome.java

--- src/com/java/strings/Palindrome.java
+++ src/com/java/strings/Palindrome.java
@@ -9,7 +9,7 @@ public class Palindrome {
         String str = sc.nextLine();
         String rev = "";
         for(int i = str.length()- 1;i >= 0;i--){
-            rev = rev + str.charAt(i);
+             rev += str.charAt(i);
         }
         if(str.equals(rev)){
             System.out.println("Palindrome");
