7c77c5d85872933f1c40dadad4ff8c1fa9c43bc2
Angasalavikas <angasalavikas123@gmail.com>
2026-08-01 10:16:47 +0530

    intial commit CopyOneToAnotherArray.java

--- src/com/java/array/CopyOneToAnotherArray.java
+++ src/com/java/array/CopyOneToAnotherArray.java
@@ -5,7 +5,6 @@ public class CopyOneToAnotherArray {
         int[] arr = {10,20,30,40,50};
         int[] arr1 = new int[arr.length];
         for( int i = 0;i < arr.length;i++){
-
             arr1[i] = arr[i];
         }
         System.out.println("copy array");
